59b55d21f1a4a02cb39e78a7738f6c695cc8de37
caltinsoy <cemalettin_14723@hotmail.com>
2021-06-21 23:51:24 +0300

    Simple Spring Boot, PostgreSql integration with  Rest API.

---
+++ src/main/java/com/example/postgrelearn/PostgrelearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgrelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgrelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgrelearnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/postgrelearn/model/Tutorial.java
@@ -0,0 +1,31 @@
+package com.example.postgrelearn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+}
